4c7f08637ba3bacbe24a2fe0b1f0953193dd97e0
putuangga <iputuanggakrisna@gmail.com>
2025-01-11 01:19:22 +0700

    fix: maybe

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,50 @@
-import commonjs from "@rollup/plugin-commonjs"
-import typescript from "@rollup/plugin-typescript"
-import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external"
-import resolve from "@rollup/plugin-node-resolve"
-import terser from "@rollup/plugin-terser"
-import dts from "rollup-plugin-dts"
-import postcss from "rollup-plugin-postcss"
-
-const packageJson = require("./package.json")
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import dts from "rollup-plugin-dts";
+import url from "@rollup/plugin-url";
+import postcss from "rollup-plugin-postcss";
+import packageJson from "./package.json";
 
 export default [
-    {
-        input: "src/index.ts",
-        output: [
-            {
-                file: packageJson.main,
-                format: "cjs",
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: "esm",
-                sourcemap: true,
-            }
-        ],
-        plugins: [
-            PeerDepsExternalPlugin(),
-            resolve(),
-            commonjs,
-            typescript({tsconfig: "./tsconfig.json"}),
-            terser(),
-            postcss(),
-        ],
-        external: ["react", "react-dom", 'react/jsx-runtime']
-    },
-    {
-        input: "src/index.ts",
-        output: [
-            {
-                file: packageJson.types
-            }
-        ],
-        plugins: [dts.default()],
-        external: [/\.css/]
-    }
-]
+  {
+    input: "src/index.tsx",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+      },
+    ],
+    plugins: [
+      PeerDepsExternalPlugin(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
+      postcss(),
+      url({
+        include: ['src/fonts/**/*.woff2'],
+        limit: 0,                 
+        destDir: 'dist/fonts/',  
+        publicPath: '/dist/fonts/', 
+      }),
+    ],
+    external: ["react", "react-dom", "react/jsx-runtime"],
+  },
+  {
+    input: "src/index.tsx",
+    output: [
+      {
+        file: packageJson.types,
+      },
+    ],
+    plugins: [dts.default()],
+    external: [/\.css/],  
+  },
+];
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import Viewer from "./components"
-
-export default Viewer
